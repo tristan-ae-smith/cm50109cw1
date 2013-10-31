9abb31e3af99f5af11da0ea843d6149e5bbbe014
taes1g09 <thomas.ae.smith@googlemail.com>
2013-10-31 16:59:53 +0000

    Probably pretty much finished? Need to implement and perform testing.

--- cw1.c
+++ cw1.c
@@ -1,66 +1,232 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 
 #define ON_LENGTH 64
 #define FN_LENGTH 64
+#define DEBUG 1
+
+void errlog(char message[]) {
+	if (DEBUG) {
+		printf("%s", message);
+	}
+}
 
 typedef struct node {
 	struct node *left, *right;
-	int age;
-	char fName[ON_LENGTH]; //TODO validate assumptions
-	char oName[FN_LENGTH];
 	int code;
+	int age;
+	char fName[FN_LENGTH]; //firstName		//TODO validate assumptions
+	char oName[ON_LENGTH]; //otherNames
 } node;
 
 //'comparator' is now a pointer to a function
 // takes two node pointers and returns an int
-// -1 for <; 0 for ==; 1 for >
+// -ve for <; 0 for ==; +ve for >
 typedef int (*comparator)(node*, node*);
-
-typedef struct tree {
-	node *root;
-	comparator sortType;
-} tree;
 
 int byAge(node *x, node *y) {
 	return x->age - y->age;
 }
 
-//add a node
+int byCode(node *x, node *y) {
+	return x->code - y->code;
+}
+
+int byFirst(node *x, node *y) {
+	return strncmp(x->fName, y->fName, FN_LENGTH);
+}
+
+int byLast(node *x, node *y) {
+	return strncmp(x->oName, y->oName, ON_LENGTH);
+}
+
+
+//'operator' is now a pointer to a function
+// takes a node pointer and performs some operation
+typedef void (*operator)(node*);
+
+//operator to print the given node
+void printer(node *p) {
+	printf("%d %d %s %s", p->code, p->age, p->fName, p->oName);
+	return;
+}
+
+//'visitor' is now a pointer to a recursive function
+// takes a node pointer and a function pointer
+// visits all nodes beneath that node in appropriate order
+// and performs the operator
+typedef void (*visitor)(node*, operator);
+
+//visitor to perform left-to-right traversal of the tree
+void traverse(node *n, operator op) {
+	if (n->left != NULL) {
+		traverse(n->left, op);
+	}
+	op(n);
+	if (n->right != NULL) {
+		traverse(n->right, op);
+	}
+}
+
+//visitor to perform right-to-left (reverse) traversal of the tree
+void reverse(node *n, operator op) {
+	if (n->right != NULL) {
+		reverse(n->right, op);
+	}
+	op(n);
+	if (n->left != NULL) {
+		reverse(n->left, op);
+	}
+}
+
+typedef struct tree {
+	node *root;
+	comparator sortType;
+	visitor visitOrder;
+} tree;
+
+node* makeNode(int code, int age, char *fName, char *oName) {
+	node *n = malloc(sizeof(node));
+	n->code = code;
+	n->age = age;
+	strcpy(n->fName, fName);
+	strcpy(n->oName, oName);
+	// n->fName = fName;
+	// n->oName = oName;
+	n->left = n->right = NULL;
+	return n;
+}
+
+//add a node 		//TODO make this use a pointer-to-pointer
 tree* addNode(tree *t, node *n) {
+	printf("Adding node %d\n", n->code);
 	if (t->sortType == NULL) {
 		printf("Node %d added to uninitialised tree; discarded.\n", n->code);
 		return t;
+	}
+
+	if (t->root == NULL) {
+		t->root = n;
+		return t;
+	}
+
+	node *parent = t->root;
+	node *next = parent;
+
+	//iterate down into the tree until we find an empty pointer in the right place
+	while (next != NULL) {
+		parent = next;
+		next = ( t->sortType(parent, n) > 0 )? parent->left : parent->right;
+	}
+	
+	if ( t->sortType(parent, n) > 0 ) {
+		parent->left = n;
+	} else {
+		parent->right = n;
 	}
 
 	//return tree for future chaining
 	return t;
 }
 
+void printHelp() {
+	printf("cw1:\tread, merge and sort data records from files\n");
+	printf("Usage: cw1 file1 <file2 file3 ..> -[flags]\n");
+	printf("\t Reads file1 and optional additional files\n");
+	printf("\t Sorts by age and prints to standard output\n");
+	printf("Flags:\n");
+	printf("\t-r\treverse output order\n");
+	printf("\t-c\tsort by code\n");
+	printf("\t-f\tsort by first name\n");
+	printf("\t-l\tsort by last name\n");
+	printf("\t-h\tprint this help\n");
+	return;
+}
+
+void runTests() {
+
+}
+
 int main(int argc, char *argv[])
 {
-	// tree *people = malloc(sizeof(tree));
-	// people->comparator = &byAge;
+	if (argc == 1) {
+		printHelp();
+		return 0;
+	}
+
+	printf("Initialising\n");
+	tree *people = malloc(sizeof(tree));
+	people->sortType = &byAge;
+	people->visitOrder = &traverse;
+
+printf("Tree created\n");
 
 	int i;
 	for (i = 1; i < argc; ++i)
 	{
-		FILE *file;
-		int code, age;
-		char fName[FN_LENGTH], oName[ON_LENGTH];
+		if (argv[i][0] == '-') {
+			/* deal with flags */
+			int j = 1;
+			while (argv[i][j] != '\0') {
+				char flag = tolower(argv[i][j]);
 
-		if ( (file = fopen(argv[i], "r")) == NULL) {
-			printf("Can't open %s\n", argv[i]);
-			return 1;
-		}
-
-		while (!feof(file)) {
-			if (fscanf(file, "%d %d %s ", &code, &age, fName) != 3) {
-				break;
+				switch (flag) {
+					case 'h':
+						printHelp();
+						return 0;
+					case 't':
+						runTests();
+						return 0;
+					case 'r':
+						people->visitOrder = &reverse;
+						break;
+					case 'c':
+						people->sortType = &byCode;
+						break;
+					case 'f':
+						people->sortType = &byFirst;
+						break;
+					case 'l':
+						people->sortType = &byLast;
+						break;
+					default: 
+						printf("Unrecognised flag: %c\n", flag);
+				}
+				j++;
 			}
-			fgets(oName, ON_LENGTH, file);
-			printf("%d %d %s %s", code, age, oName, oName);
 		}
 	}
-	// do something with the args, maybe
+
+printf("Flags checked\n");
+
+	for (i = 1; i < argc; ++i)
+	{
+		if (argv[i][0] != '-')
+		{
+			printf("Processing %s\n", argv[i]);
+			FILE *file;
+			int code, age;
+			char fName[FN_LENGTH], oName[ON_LENGTH];
+
+			if ( (file = fopen(argv[i], "r")) == NULL) {
+				printf("Can't open %s\n", argv[i]);
+				return 1;
+			}
+
+			while (!feof(file)) {
+				if (fscanf(file, "%d %d %s ", &code, &age, fName) != 3) {
+					break;
+				}
+				fgets(oName, ON_LENGTH, file);
+				addNode(people, makeNode(code, age, fName, oName));
+				printf("%d %d %s %s", code, age, fName, oName);
+			}
+		}
+	}
+
+	people->visitOrder(people->root, &printer);
+
 	return 0;
 }
